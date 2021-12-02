39e39be2788ceec0cb1865dcf943b318ae924861
unknown <roselandambuku@gmail.com>
2021-12-02 21:30:52 +0300

    variables in python

---
+++ Variables/variables.py
@@ -0,0 +1,22 @@
+
+# Variables in Python
+
+first_name = 'Roseland'
+last_name = 'Ambuku'
+country = 'Kenya'
+city = 'Kisumu'
+age = 22
+is_in_relationship = False
+hobbies = ['Reading, Coding, Blogging, Gardening']
+
+#Printing the values stored in the variables
+print("First Name :",first_name)
+print("Last Name :",last_name)
+print("Country :",country)
+print("City :",city)
+print("Age :",age)
+print("Relationship Status :",is_in_relationship)
+print("Hobbies :", hobbies)
+
+#Printing the values stored in the variables using f string
+print(f"I am {first_name} {last_name} and I come from {city},{country}. I am {22} years old and my hobbies include {hobbies}")
