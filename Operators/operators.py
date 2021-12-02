821a82452cb1fa374fdcda6c1d288bbb704d17d7
unknown <roselandambuku@gmail.com>
2021-12-02 22:04:41 +0300

    operators in python

---
+++ Operators/operators.py
@@ -0,0 +1,16 @@
+import cmath
+#Operators in python
+
+#age as an integer
+age = 22
+
+#length as a float
+length = 155.6
+
+#creating a complex number
+i= 7
+j= 9
+k= complex(i,j)
+print(k)
+
+#area of a triangle with use input
