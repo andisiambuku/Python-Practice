89054b163324432ad1e8fde276533ab80448cc66
unknown <roselandambuku@gmail.com>
2021-12-12 11:25:34 +0300

    conditionals in python

---
+++ Conditionals/conditionals.py
@@ -0,0 +1,12 @@
+# Conditionals in Python
+
+
+age = int(input('Enter your age:'))
+
+# age = 18
+
+if age > 18 or age == 18:
+    print('You are old enough to learn to drive')
+else:
+    print('You are too young to learn to drive')
+
