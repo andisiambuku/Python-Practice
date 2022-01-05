0eb429db9d7c13ae6150637fedb39a7c6e85172a
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-01-05 20:31:33 +0300

    functions in python

--- Functions/functions.py
+++ Functions/functions.py
@@ -7,13 +7,22 @@ def add_two_numbers(num_one, num_two):
 print('Sum of two numbers:',add_two_numbers(2,4))
 
 '''Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle'''
-def area_of_circle(pie,radius):
-    area = pie *radius*radius
+def area_of_circle(radius):
+    pie = 3.14
+    area = pie *radius**2
     return area
-print('Area of a circle:', area_of_circle(3.14,7))
+print('Area of a circle:', area_of_circle(7))
 
 '''Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer'''
 def check_season(month):
-    season = month + 'Autumn','Winter','Spring','Summer'
-    return season
-print('The season in the month', check_season('August'))
+    seasons= ['Autumn','Spring','Summer','Winter']
+    if month in ('December','January','February',):
+        print('Winter')
+    elif month in ('March','April','May'):
+        print('Spring')
+    elif month in ('June','July','August'):
+        print('Summer')
+    else :
+        print('Autumn')
+    return seasons
+print('The season is',check_season('April'))
