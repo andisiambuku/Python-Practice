ebf00fb9c2994e395d674ffc52fbf0faec7be003
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-01-05 19:47:06 +0300

    functions in python

---
+++ Functions/functions.py
@@ -0,0 +1,7 @@
+#Functions in Python
+
+'''Declare a function add_two_numbers. It takes two parameters and it returns a sum'''
+def add_two_numbers(num_one, num_two):
+    sum = num_one + num_two
+    return sum
+print('Sum of two numbers:',add_two_numbers(2,4))
