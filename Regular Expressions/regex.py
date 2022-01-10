9629195e4a8ce3e5b4350098954a7b36c361166c
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-01-10 14:45:37 +0300

    regex in python

--- Regular Expressions/regex.py
+++ Regular Expressions/regex.py
@@ -1,3 +1,7 @@
 #Regular Expressions in Python
 
 import re
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+text = re.sub('[%$&;!@#]','',sentence)
+print(text)
