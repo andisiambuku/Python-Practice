9da2b971666faca817bca3b8d30f87444cfc5485
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2022-01-11 22:26:15 +0300

    python package manager

--- Python Package Manager/pythonpackagemanager.py
+++ Python Package Manager/pythonpackagemanager.py
@@ -1,6 +1,11 @@
 import requests
+from collections import Counter
 
 url = 'http://www.gutenberg.org/files/1112/1112.txt'
 response = requests.get(url)
 print(response)
 print(response.text)
+text_set = response.text
+text_list = text_set.split()
+text_count = Counter(text_list)
+print(text_count.most_common(10))
